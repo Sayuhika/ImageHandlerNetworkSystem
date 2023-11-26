57fbb14853658dcea20a4de11c66c06da5fe7c91
xxMelkorxx <xxxserborxxx@gmail.com>
2023-11-26 20:25:04 +0300

    feat: attempt to implement a collector

--- apps/ics-app/src/ICSMain.cpp
+++ apps/ics-app/src/ICSMain.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 #include <zmq.hpp>
 #include <Message.h>
 #include <CommsHandlers.hpp>
+
+bool compareByTime(const CH::AoiMsg& a, const CH::AoiMsg& b)
+{
+    return std::stoi(a.time) < std::stoi(b.time);
+}
 
 int main(int argc, char* argv[]) 
 {
@@ -20,8 +26,9 @@ int main(int argc, char* argv[])
     socket.bind("tcp://" + addr);
 
     std::vector<CH::AoiMsg> messages;
+    cv::Mat orig, proc;
 
-    while (true) 
+    for (int i = 0;;i++)
     {
         zmq::message_t zmq_msg;
         CH::AoiMsg message_img;
@@ -31,11 +38,21 @@ int main(int argc, char* argv[])
         {
             messages.push_back(message_img);
 
-            /// TO DO BEGIN
-
-            // parsing to mat from 3 uint RGB channels
-            // to do somthing with message_img
-
+            // TO DO BEGIN
+            if (i > 60)
+            {
+                std::sort(messages.begin(), messages.end(), compareByTime);
+                std::string time = messages[0].time;
+                orig = CH::ImageToMat(messages[0].aoi[0]); // получение оригинала
+                proc = CH::ImageToMat(messages[0].aoi[1]); // получение обработанной картинки
+                messages.erase(messages.cbegin(), messages.cbegin() + 1);
+                
+                // Отображение картинки
+                cv::imshow(time, proc);
+                char c = cv::waitKey(33); // 33 мск оптимально для 30 кдаров в секунду
+                if (c == 's') // остановка цикла
+                    break;
+            }
             /// TO DO END
         }
         else
